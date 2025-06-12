3ef739f3c7888f1e5826de7643b8008dd24e1532
Bryan Gracida Tapia <bryangtapia@gmail.com>
2025-06-12 11:39:11 -0600

    - Se creo el modulo configurations para incluir todas las configuraciones del juego.

---
+++ version_02/Configurations.py
@@ -0,0 +1,38 @@
+"""
+Nombre del equipo: Átomos
+Integrantes: Bryan Gracida Tapia, Addi Toro Chávez
+fecha: 13 de mayo del 2025
+"""
+class Configurations:
+    """
+    Clase que contiene todas las configuraciones del juego.
+    """
+    #Configuraciones de la pantalla
+    _screen_size = (1280, 720)  # Resolución de la pantalla (ancho,alto)
+    _game_title = "Rebel Rescue"  #Título del juego
+    _background = (50, 50, 50)  # Fondo de la pantalla en formato RGB
+
+
+    @classmethod
+    def get_screen_size(cls)->tuple[int,int]:
+        """
+        Getter para screen_size.
+        :return:
+        """
+        return  cls._screen_size
+
+    @classmethod
+    def get_game_title(cls)->str:
+        """
+        Getter para _game_title.
+        :return:
+        """
+        return  cls._game_title
+
+    @classmethod
+    def get_background(cls)->tuple[int,int,int]:
+        """
+        Getter para background.
+        :return:
+        """
+        return  cls._background
